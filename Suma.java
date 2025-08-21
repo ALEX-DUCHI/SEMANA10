edfc5d54606e2e39705d9ed8487f750a45dc6311
ALEX-DUCHI <ae.duchid@uea.edu.ec>
2025-08-20 23:30:41 -0500

    Add files via upload

---
+++ Suma.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.suma;
+
+/**
+ *
+ * @author User
+ */
+public class Suma {
+
+    public static void main(String[] args) {
+        int num =0;
+        for(int i=1; i<=10;i++){
+        num +=i;
+        }
+        System.out.println("LA SUMA DE LOS 10 NUMEROS POSITIVOS ES " +num);
+    }
+}
